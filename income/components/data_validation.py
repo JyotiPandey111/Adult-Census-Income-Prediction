c36757a6f9f6ea782ed67ccfa7ce083f4699dda1
JyotiPandey111 <jyotipandeydip@gmail.com>
2024-05-07 11:42:10 +0530

    Detecting data drift in data validation

--- income/components/data_validation.py
+++ income/components/data_validation.py
@@ -85,6 +85,9 @@ class DataValidation:
     
 
     def detect_dataset_drift(self,base_df,current_df,threshold=0.05)->bool:
+        '''
+        To detect data drift
+        '''
         try:
             status=True
             report ={}
