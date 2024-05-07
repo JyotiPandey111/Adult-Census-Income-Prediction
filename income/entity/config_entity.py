623f366bcde6fb9d844cf6e7c321f5937c99ce12
JyotiPandey111 <jyotipandeydip@gmail.com>
2024-05-07 09:15:49 +0530

    setting config entity for validation of data

--- income/entity/config_entity.py
+++ income/entity/config_entity.py
@@ -35,7 +35,6 @@ class DataIngestionConfig:
 
 class DataValidationConfig:
 
-
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
         self.valid_data_dir: str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
