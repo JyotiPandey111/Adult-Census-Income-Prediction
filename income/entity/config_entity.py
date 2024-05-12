6a0de6294f8a27e126c0991bb4faadd73bee03a5
JyotiPandey111 <jyotipandeydip@gmail.com>
2024-05-12 13:01:03 +0530

    added data transformation config

--- income/entity/config_entity.py
+++ income/entity/config_entity.py
@@ -12,6 +12,9 @@ class TrainingPipelineConfig:
         self.timestamp: str = timestamp
 
 class DataIngestionConfig:
+        '''
+        class to assign data ingestion configuration
+        '''
         def __init__(self,training_pipeline_config:TrainingPipelineConfig):
             self.data_ingestion_dir: str = os.path.join(
                 training_pipeline_config.artifact_dir, training_pipeline.DATA_INGESTION_DIR_NAME
@@ -34,6 +37,9 @@ class DataIngestionConfig:
 
 
 class DataValidationConfig:
+    '''
+    class assign data validation configuration
+    '''
 
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join( training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
@@ -50,9 +56,10 @@ class DataValidationConfig:
         )
 
 
-
-
 class DataTransformationConfig:
+    '''
+    Class to assign data transformations configuration 
+    '''
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_transformation_dir: str = os.path.join( training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME )
         self.transformed_train_file_path: str = os.path.join( self.data_transformation_dir,training_pipeline.DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR,
